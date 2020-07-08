0926893c97c063de5749632670c3087b21f3201e
jianzhnie <jianzhnie@126.com>
2020-07-08 19:31:25 +0800

    add custom loading

--- configs_custom/seg/seg_voc/seg_voc1.py
+++ configs_custom/seg/seg_voc/seg_voc1.py
@@ -1,6 +1,6 @@
 # model settings
 norm_cfg = dict(type='SyncBN')
-input_size = 513
+input_size = 512
 model = dict(
     type='FCN',
     pretrained='torchvision://resnet50',
@@ -38,55 +38,48 @@ test_cfg = None
 dataset_type = 'VOCSegmentation'
 data_root = '/data/voc/VOCdevkit/'
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375])
-ignore_label = 255
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=False, with_label=False, with_mask=False, with_seg=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomCrop', crop_size=input_size),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
-        dataset=dict(
-            type=dataset_type,
-            data_root=data_root,
-            imglist_name=data_root + 'VOC2012/ImageSets/Segmentation/train.txt',
-            img_prefix=data_root + 'VOC2012/',
-            seg_prefix=data_root + 'VOC2012/',
-        ),
-        transforms=[
-            dict(type='RandomScale', min_scale=0.5, max_scale=2.0, mode='bilinear'),
-            dict(type='RandomCrop', height=513, width=513, image_value=img_norm_cfg['mean'], mask_value=ignore_label),
-            dict(type='RandomRotate', p=0.5, degrees=10, mode='bilinear', border_mode='constant', image_value=img_norm_cfg['mean'], mask_value=ignore_label),
-            dict(type='GaussianBlur', p=0.5, ksize=7),
-            dict(type='HorizontalFlip', p=0.5),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='ToTensor'),
-        ],
-    ),
-    val=dict(
-        dataset=dict(
-            type=dataset_type,
-            data_root=data_root,
-            imglist_name=data_root + 'VOC2012/ImageSets/Segmentation/val.txt',
-            img_prefix=data_root + 'VOC2012/',
-            seg_prefix=data_root + 'VOC2012/',
-        ),
-        transforms=[
-            dict(type='PadIfNeeded', height=513, width=513, image_value=img_norm_cfg['mean'], mask_value=ignore_label),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='ToTensor'),
-        ],
-    ),
+        type=dataset_type,
+        data_root=data_root,
+        imglist_name=data_root + 'VOC2012/ImageSets/Segmentation/train.txt',
+        img_prefix=data_root + 'VOC2012/JPEGImages/',
+        seg_prefix=data_root + 'VOC2012/SegmentationClass/',
+        pipeline=train_pipeline),
     test=dict(
         type=dataset_type,
-        imglist_name=data_root + 'VOC2012/ImageSets/Segmentation/val.txt',
-        img_prefix=data_root + 'VOC2012/',
-        seg_prefix=data_root + 'VOC2012/',
-        transforms=[
-            dict(type='PadIfNeeded', height=513, width=513, image_value=img_norm_cfg['mean'], mask_value=ignore_label),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='ToTensor'),
-        ],
-    ),
-)
+        ann_file=data_root + 'VOC2012/ImageSets/Segmentation/val.txt',
+        img_prefix=data_root + 'VOC2012/JPEGImages/',
+        seg_prefix=data_root + 'VOC2012/SegmentationClass/',
+        pipeline=test_pipeline))
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
