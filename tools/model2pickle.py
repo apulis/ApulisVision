852e63b91d2a656c7c86cae4fad4729c8fa3bbf4
jianzhnie <jianzhnie@126.com>
2020-10-27 15:39:58 +0800

    change code for kfserving

--- tools/model2pickle.py
+++ tools/model2pickle.py
@@ -101,12 +101,12 @@ def custom_inference_detector(model, img):
         
     # Process detection mask
     bboxes = np.vstack(bbox_result)
+    bboxes[:, [0,1,2,3]] = bboxes[:, [1,0,3,2]]
     labels = [
-        np.full(bbox.shape[0], i, dtype=np.int32)
+        np.full(bbox.shape[0], i+1, dtype=np.int32)
         for i, bbox in enumerate(bbox_result)
     ]
     labels = np.concatenate(labels)
-
     output_dict = {}
     output_dict['num_detections'] = bboxes.shape[0]
     output_dict['detection_classes'] = labels
@@ -120,7 +120,7 @@ def custom_inference_detector(model, img):
     return output_dict
 
 
-def dump_infer_model(checkpoint_file, config_file, output_file, target='det', device='cuda:0'):
+def dump_infer_model(checkpoint_file, config_file, output_file, labelfile, target='det', device='cuda:0'):
     print("------------------------------")
     print("START EXPORT MODEL")
     print("------------------------------")
@@ -134,6 +134,7 @@ def dump_infer_model(checkpoint_file, config_file, output_file, target='det', de
         model = init_segmentor(config=config_file, checkpoint=checkpoint_file, device=device)
         infer_model = InferenceModel(model, custom_inference_segmentor)
     pickle_dump(infer_model, output_file)
+    writeLabels(infer_model.model.CLASSES,  labelfile)
     # model_infer(output_file)
     print("------------------------------")
     print("SUCCESS EXPORT MODEL")
@@ -142,25 +143,37 @@ def dump_infer_model(checkpoint_file, config_file, output_file, target='det', de
 
 from PIL import Image
 import io
+import os
+import json
 
 
 def model_infer(pickle_file, img_bytes):
     img = Image.open(io.BytesIO(img_bytes))
     inputImg = np.asarray(img)
     infer_model = pickle_load(pickle_file)
+    print(infer_model.model.CLASSES)
     result = infer_model.predict(infer_model.model, inputImg)
     print(pickle_file)
     print(result)
+
+
+def writeLabels(CLASSES, label_file):
+    data = []
+    with open(label_file, 'w') as jsonfile:
+        for key, value in enumerate(CLASSES, start=1):
+            data.append({"id": int(key), "display_name": value})
+        json.dump(data, jsonfile)
 
 
 if __name__ == '__main__':
     # dump model
     config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
     checkpoint_file = 'work_dirs/faster-rcnn/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-    output_file = 'work_dirs/faster-rcnn/det.pkl'
-    dump_infer_model(checkpoint_file, config_file, output_file,  target='det')
+    output_file = 'work_dirs/faster-rcnn/export_model.pkl'
+    label_file =  'work_dirs/faster-rcnn/class_names.json'
+    dump_infer_model(checkpoint_file, config_file, output_file, label_file, target='det')
     img_file = 'demo/demo.jpg'
     img_bytes = open(img_file, 'rb').read()
-    model_infer("work_dirs/faster-rcnn/det.pkl", img_bytes)
+    model_infer("work_dirs/faster-rcnn/export_model.pkl", img_bytes)
     # model_infer("work_dir/cls/export_model.pkl", img_bytes)
     # model_infer("work_dir/seg/export_model.pkl", img_bytes)
